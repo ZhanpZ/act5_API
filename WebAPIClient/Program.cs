5463c1e7f3dafaa9dae41258a8d3ec9728b62b97
ZhanpZ <66219129+ZhanpZ@users.noreply.github.com>
2022-12-18 14:54:56 -0500

    Add files via upload

---
+++ WebAPIClient/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+private static readonly HttpClient client = new HttpClient();
+
+//namespace WebAPIClient;
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        await ProcessRepositories();
+    }
+    private static async Task ProcessRepositories()
+    {
+        while(true)
+        {
+            Console.WriteLine("Enter name of pokemon. Enter key for exit");
+
+            var pokemonName = Console.ReadLine();
+
+            if(string.IsNullOrEmpty(pokemonName))
+            {
+                break;
+            }
+
+            var result = await client.GetAsync("https://pokeapi.co/api/v2/pokemon/" + pokemonName.ToLower());
+            var resultRead = await result.Content.ReadAsStringAsync();
+
+            //Console.WriteLine(resultRead);
+
+        }
+        catch(Exception)
+        {
+            Console.WriteLine("ERROR, enter a correct name");
+        }
+
+    }
+}
